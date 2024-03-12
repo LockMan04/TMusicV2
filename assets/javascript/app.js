1d7bc069d37cd50ab37983f93e6ddffa6121885c
Thanh Toàn <2200007030@nttu.edu.vn>
2024-03-13 06:18:17 +0700

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+const userIcon = document.querySelector(".hyper-link-item:last-child");
+const userMenu = document.querySelector(".user-menu");
+
+userIcon.addEventListener("click", () => {
+  userMenu.classList.toggle("hidden");
+
+  document.addEventListener("click", (e) => {
+    if (!userMenu.contains(e.target) && !userIcon.contains(e.target)) {
+      userMenu.classList.add("hidden");
+    }
+  });
+});
+
+// Chữ Chạy
+const typedEffect = new Typed("#multiText", {
+  strings: [
+    "TMusic AppWeb Music Số 1 Việt Nam",
+    "Hello",
+    "Chill Tý Nhạc Nhé",
+    "Khò Khò",
+    "Cà Nhính Cà Nhính",
+    "Anh Yêu Bé Nhất",
+  ],
+  loop: true,
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 10000,
+  sunffle: true,
+  showCursor: false,
+});
+
+/*****************
+ * Scroll
+ */
+const list = document.querySelectorAll(".for-you-list");
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+list.forEach((list) => {
+  list.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - list.offsetLeft;
+    scrollLeft = list.scrollLeft;
+    list.style.cursor = "grabbing";
+  });
+
+  list.addEventListener("mouseup", () => {
+    isDragging = false;
+    list.style.cursor = "grab";
+  });
+
+  list.addEventListener("mouseleave", () => {
+    isDragging = false;
+    list.style.cursor = "grab";
+  });
+
+  list.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - list.offsetLeft;
+    const walk = x - startX; // Multiplier để tăng tốc độ kéo
+    list.scrollLeft = scrollLeft - walk;
+  });
+});
